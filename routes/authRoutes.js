43f3263ef421f6b6a6b8c3a45298baf5a99c9302
Kedhareswar <129578151+Kedhar-mt@users.noreply.github.com>
2025-03-04 15:17:06 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { 
-  register, 
-  login, 
+const {
+  register,
+  login,
   forgotPassword,
-  resetPassword ,
+  resetPassword,
   refreshToken,
-  logout
+  logout,
+  forceLogout
 } = require('../controllers/authController');
-const authenticate = require('../middleware/authenticate');
-
+const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', authenticate,logout);
+router.post('/logout', verifyToken, logout);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
 router.post('/refresh-token', refreshToken);
-
-
+router.post('/force-logout/:userId', verifyToken, forceLogout);
 
 module.exports = router;
